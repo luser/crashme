683ba10ad99b9fff7621db3ac14c82ae39327b0a
ted.mielczarek@gmail.com <ted.mielczarek@gmail.com@c8c91368-d149-0410-948e-53f8f13d1be3>
2012-10-12 18:46:12 +0000

    Rework crashme into a bootstrap extension

--- resources/content/contentscript.js
+++
@@ -1,8 +0,0 @@
-// Just a sanity check
-if (!Util.isParentProcess()) {
-  Components.utils.import("resource://gre/modules/ctypes.jsm");
-  //TODO: could load the crasher library and crash in other ways
-  var zero = new ctypes.intptr_t(8);
-  var badptr = ctypes.cast(zero, ctypes.PointerType(ctypes.int32_t));
-  var crash = badptr.contents;
-}

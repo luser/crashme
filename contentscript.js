ed7e9f1540a4082ca51063e1f46c347dd0cd9b68
Ted Mielczarek <ted@mielczarek.org>
2015-03-12 12:58:52 -0400

    Switch everything to MPL2 license

--- contentscript.js
+++ contentscript.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 // Just a sanity check
 let appInfo = Components.classes["@mozilla.org/xre/app-info;1"];
 if (appInfo && appInfo.getService(Components.interfaces.nsIXULRuntime)

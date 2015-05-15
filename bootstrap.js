cd4c7ea57492563d8d6b20c5f97c43c6e69efdba
Ted Mielczarek <ted@mielczarek.org>
2015-05-15 10:33:53 -0400

    Make crash_content use the current tab instead of loading a new one. Also do some random cleanup.

--- bootstrap.js
+++ bootstrap.js
@@ -1,6 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+/*global Components, Services, Crasher, XPCOMUtils, MetroUtils, CustomizableUI */
 
 const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
 Cu.import("resource://gre/modules/Services.jsm");
@@ -14,15 +15,11 @@ Cu.import("resource://gre/modules/Services.jsm");
  *   for future window-opens.
  */
 function watchWindows(callback, option) {
-  // Wrap the callback in a function that ignores failures
   function watcher(window) {
-    //try {
-      // Now that the window has loaded, only handle browser windows
-      let {documentElement} = window.document;
-      if (documentElement.getAttribute("windowtype") == "navigator:browser")
-        callback(window);
-    //}
-    //catch(ex) {}
+    // Now that the window has loaded, only handle browser windows
+    let {documentElement} = window.document;
+    if (documentElement.getAttribute("windowtype") == "navigator:browser")
+      callback(window);
   }
 
   // Wait for the window to finish loading before running the callback
@@ -86,6 +83,12 @@ function unload(callback, container) {
     return;
   }
 
+  function removeUnloader() {
+    let index = unloaders.indexOf(unloader);
+    if (index != -1)
+      unloaders.splice(index, 1);
+  }
+
   // The callback is bound to the lifetime of the container if we have one
   if (container != null) {
     // Remove the unloader when the container unloads
@@ -96,7 +99,7 @@ function unload(callback, container) {
     callback = function() {
       container.removeEventListener("unload", removeUnloader, false);
       origCallback();
-    }
+    };
   }
 
   // Wrap the callback in a function that ignores failures
@@ -107,87 +110,80 @@ function unload(callback, container) {
     catch(ex) {}
   }
   unloaders.push(unloader);
-
-  // Provide a way to remove the unloader
-  function removeUnloader() {
-    let index = unloaders.indexOf(unloader);
-    if (index != -1)
-      unloaders.splice(index, 1);
-  }
-  return removeUnloader;
 }
 
 function crash() {
-    //TODO: expose other options?
-    let how = Crasher.CRASH_NULL_POINTER_DEREF;
-    Crasher.crash(how);
+  //TODO: expose other options?
+  let how = Crasher.CRASH_NULL_POINTER_DEREF;
+  Crasher.crash(how);
 }
 
 function crash_content() {
   let wm = Cc["@mozilla.org/appshell/window-mediator;1"].
         getService(Ci.nsIWindowMediator);
   let win = wm.getMostRecentWindow("navigator:browser");
-  let tab = win.gBrowser.addTab("http://mozilla.org");
-  tab.linkedBrowser.messageManager.loadFrameScript("resource://crashme/contentscript.js", true);
+  let browser = win.gBrowser.selectedBrowser;
+  if (browser.isRemoteBrowser) {
+    browser.messageManager.loadFrameScript("resource://crashme/contentscript.js", true);
+  } else {
+    // Could try harder and force-load an e10s window or something.
+  }
 }
 
 let menuIDs = new WeakMap();
 let metroSettingsPanelEntryId;
 
 function addUI(window) {
-    let document = window.document;
-    if (Services.appinfo.ID == "{aa3c5121-dab2-40e2-81ca-7ea25febc110}") {
-        // Android Fennec
-        menuIDs.set(window,
-                    window.NativeWindow.menu.add("Crash me!", null, function() {
-                        crash();
-                    }));
-    } else {
-        let menu = document.getElementById("menu_ToolsPopup");
-        if (!menu) {
-            return;
-        }
-        let menuitem = document.createElement("menuitem");
-        menuitem.setAttribute("id", "crashme-crashme");
-        menuitem.setAttribute("label", "Crash me!");
-        menuitem.addEventListener("command", function() {
-            crash();
-        });
-        menu.appendChild(menuitem);
+  let document = window.document;
+  if (Services.appinfo.ID == "{aa3c5121-dab2-40e2-81ca-7ea25febc110}") {
+    // Android Fennec
+    menuIDs.set(window,
+                window.NativeWindow.menu.add("Crash me!", null, function() {
+                  crash();
+                }));
+  } else {
+    let menu = document.getElementById("menu_ToolsPopup");
+    if (!menu) {
+      return;
     }
-  if (Services.appinfo.ID == "{ec8030f7-c20a-464f-9b0e-13a3a9e97384}") {
-
+    let menuitem = document.createElement("menuitem");
+    menuitem.setAttribute("id", "crashme-crashme");
+    menuitem.setAttribute("label", "Crash me!");
+    menuitem.addEventListener("command", function() {
+      crash();
+    });
+    menu.appendChild(menuitem);
   }
 }
 
 function removeUI(window) {
-    if (Services.appinfo.ID == "{aa3c5121-dab2-40e2-81ca-7ea25febc110}") {
-        if (menuIDs.has(window)) {
-            window.NativeWindow.menu.remove(menuIDs.get(window));
-            menuIDs.delete(window);
-        }
-    } else {
-        let document = window.document;
-        let item = document.getElementById("crashme-crashme");
-        if (item) {
-            item.parentNode.removeChild(item);
-        }
+  if (Services.appinfo.ID == "{aa3c5121-dab2-40e2-81ca-7ea25febc110}") {
+    if (menuIDs.has(window)) {
+      window.NativeWindow.menu.remove(menuIDs.get(window));
+      menuIDs.delete(window);
     }
+  } else {
+    let document = window.document;
+    let item = document.getElementById("crashme-crashme");
+    if (item) {
+      item.parentNode.removeChild(item);
+    }
+  }
 }
 
 function addPrefUI(document) {
-    var button = document.getElementById("crashme-crash-button");
-    button.addEventListener("click", function() {
-        crash();
-    });
+  var button = document.getElementById("crashme-crash-button");
+  button.addEventListener("click", function() {
+    crash();
+  });
 }
 
 function addMetroCharmsUI() {
   // Firefox for Metro
   Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
   XPCOMUtils.defineLazyServiceGetter(this, "MetroUtils",
-                             "@mozilla.org/windows-metroutils;1",
-                             "nsIWinMetroUtils");
+                                     "@mozilla.org/windows-metroutils;1",
+                                     "nsIWinMetroUtils");
   try {
     Services.obs.addObserver(gObserver, "metro-settings-entry-selected", false);
     metroSettingsPanelEntryId = MetroUtils.addSettingsPanelEntry("Crash Now!");
@@ -199,7 +195,7 @@ function addMetroCharmsUI() {
 }
 
 function observer(addonID) {
-    this.addonID = addonID;
+  this.addonID = addonID;
 }
 
 observer.prototype = {
@@ -213,6 +209,7 @@ observer.prototype = {
     }
   }
 };
+
 var gObserver = null;
 var ss = null;
 var cssuri = null;
@@ -221,62 +218,62 @@ function install(data, reason) {}
 function uninstall(data, reason) {}
 
 function startup(data, reason) {
-    let resource = Services.io.getProtocolHandler("resource").QueryInterface(Ci.nsIResProtocolHandler);
-    resource.setSubstitution("crashme", data.resourceURI);
-    Cu.import("resource://crashme/modules/Crasher.jsm");
-    watchWindows(addUI);
-    gObserver = new observer(data.id);
-    Services.obs.addObserver(gObserver, "addon-options-displayed", false);
-    if (Services.appinfo.ID == "{99bceaaa-e3c6-48c1-b981-ef9b46b67d60}") {
-      addMetroCharmsUI();
-    } else if (Services.appinfo.ID == "{ec8030f7-c20a-464f-9b0e-13a3a9e97384}") {
-      let io =
-            Cc["@mozilla.org/network/io-service;1"].
-            getService(Ci.nsIIOService);
+  let resource = Services.io.getProtocolHandler("resource").QueryInterface(Ci.nsIResProtocolHandler);
+  resource.setSubstitution("crashme", data.resourceURI);
+  Cu.import("resource://crashme/modules/Crasher.jsm");
+  watchWindows(addUI);
+  gObserver = new observer(data.id);
+  Services.obs.addObserver(gObserver, "addon-options-displayed", false);
+  if (Services.appinfo.ID == "{99bceaaa-e3c6-48c1-b981-ef9b46b67d60}") {
+    addMetroCharmsUI();
+  } else if (Services.appinfo.ID == "{ec8030f7-c20a-464f-9b0e-13a3a9e97384}") {
+    let io =
+          Cc["@mozilla.org/network/io-service;1"].
+          getService(Ci.nsIIOService);
 
-      // the 'style' directive isn't supported in chrome.manifest for boostrapped
-      // extensions, so this is the manual way of doing the same.
-      ss =
-        Cc["@mozilla.org/content/style-sheet-service;1"].
-        getService(Ci.nsIStyleSheetService);
-      cssuri = io.newURI("chrome://crashme/skin/toolbar.css", null, null);
-      ss.loadAndRegisterSheet(cssuri, ss.USER_SHEET);
+    // the 'style' directive isn't supported in chrome.manifest for boostrapped
+    // extensions, so this is the manual way of doing the same.
+    ss =
+      Cc["@mozilla.org/content/style-sheet-service;1"].
+      getService(Ci.nsIStyleSheetService);
+    cssuri = io.newURI("chrome://crashme/skin/toolbar.css", null, null);
+    ss.loadAndRegisterSheet(cssuri, ss.USER_SHEET);
 
-      // Add a toolbar button
-      Components.utils.import("resource:///modules/CustomizableUI.jsm");
-      CustomizableUI.createWidget({
-        id: "toolbarbutton-crashme",
-        removable: true,
-        label: "Crash me!",
-        tooltiptext: "Crash your browser",
-        onCommand: function() {
-          crash();
-        }
-      });
+    // Add a toolbar button
+    Components.utils.import("resource:///modules/CustomizableUI.jsm");
+    CustomizableUI.createWidget({
+      id: "toolbarbutton-crashme",
+      removable: true,
+      label: "Crash me!",
+      tooltiptext: "Crash your browser",
+      onCommand: function() {
+        crash();
+      }
+    });
 
-      CustomizableUI.createWidget({
-        id: "toolbarbutton-crashme-content",
-        removable: true,
-        label: "Crash content process!",
-        tooltiptext: "Crash the content process",
-        onCommand: function() {
-          crash_content();
-        }
-      });
-    }
+    CustomizableUI.createWidget({
+      id: "toolbarbutton-crashme-content",
+      removable: true,
+      label: "Crash content process!",
+      tooltiptext: "Crash the content process",
+      onCommand: function() {
+        crash_content();
+      }
+    });
+  }
 }
 
 function shutdown(data, reason) {
-    Services.obs.removeObserver(gObserver, "addon-options-displayed");
-    gObserver = null;
-    watchWindows(removeUI, "enumerate-only");
-    Cu.unload("resource://crashme/modules/Crasher.jsm");
-    let resource = Services.io.getProtocolHandler("resource").QueryInterface(Ci.nsIResProtocolHandler);
-    resource.setSubstitution("crashme", null);
+  Services.obs.removeObserver(gObserver, "addon-options-displayed");
+  gObserver = null;
+  watchWindows(removeUI, "enumerate-only");
+  Cu.unload("resource://crashme/modules/Crasher.jsm");
+  let resource = Services.io.getProtocolHandler("resource").QueryInterface(Ci.nsIResProtocolHandler);
+  resource.setSubstitution("crashme", null);
 
-    if (metroSettingsPanelEntryId && Services.appinfo.ID == "{99bceaaa-e3c6-48c1-b981-ef9b46b67d60}") {
-      Services.obs.removeObserver(this, "metro-settings-entry-selected");
-    }
+  if (metroSettingsPanelEntryId && Services.appinfo.ID == "{99bceaaa-e3c6-48c1-b981-ef9b46b67d60}") {
+    Services.obs.removeObserver(this, "metro-settings-entry-selected");
+  }
   if (Services.appinfo.ID == "{ec8030f7-c20a-464f-9b0e-13a3a9e97384}") {
     CustomizableUI.destroyWidget("toolbarbutton-crashme");
   }
@@ -285,5 +282,4 @@ function shutdown(data, reason) {
       ss.unregisterSheet(cssuri, ss.USER_SHEET);
     }
   }
-
 }
